320a77337ab307fb82ce6f2263aaa180ac296dbd
Anna Cielas <ania.cielas@gmail.com>
2018-03-03 10:31:20 +0000

    change mongoose.connect

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes     = require("./routes/comments"),
     campgroundsRoutes = require("./routes/campgrounds"),
     authRoutes        = require("./routes/auth");
 
-//mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
-mongoose.connect("mongodb://Cynka:cynka@ds245228.mlab.com:45228/yelp_camp", {useMongoClient: true});
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
+//mongoose.connect("mongodb://Cynka:cynka@ds245228.mlab.com:45228/yelp_camp", {useMongoClient: true});
 
 
 app.use(bodyParser.urlencoded({extended: true}));
