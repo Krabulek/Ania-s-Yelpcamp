d5686269cc14a5ad7e649ddab0b1b9bb2353a0e2
Cynka <45037261+Krabulek@users.noreply.github.com>
2019-03-19 10:17:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+
+var express          = require("express"),
+    app              = express(),
+    expressSanitizer = require("express-sanitizer"),
+    bodyParser       = require("body-parser"),
+    cookieParser     = require("cookie-parser"),
+    flash            = require("connect-flash"),
+    mongoose         = require("mongoose"),
+    passport         = require("passport"),
+    LocalStrategy    = require("passport-local"),
+    methodOverride   = require("method-override"),
+    User             = require("./models/user"),
+    session          = require("express-session"),
+    seedDB           = require("./seeds");
+
+//Requiring Routes   
+var commentRoutes     = require("./routes/comments"),
+    campgroundsRoutes = require("./routes/campgrounds"),
+    authRoutes        = require("./routes/auth");
+
+
+//var url = process.env.DATABASEURL || "mongodb://Cynka:cynka@ds245228.mlab.com:45228/yelp_camp";
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+mongoose.Promise = global.Promise;
+app.use(flash());
+//seedDB();
+
+// Passport configuration
+app.use(require("express-session")({
+    secret: "No bywa",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Middleware - currentUser is available to every route
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    res.locals.warning = req.flash("warning");
+    next();
+});
+
+app.use("/", authRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundsRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp is on!");
+});
